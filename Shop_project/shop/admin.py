5f67fe491268c6b568abc927a145db431e4e55e5
ksugrvkeg23908 <135979232+ksugrvkeg23908@users.noreply.github.com>
2023-12-05 01:12:24 +0300

    added new models: Order, Tax, Discount

--- Shop_project/shop/admin.py
+++ Shop_project/shop/admin.py
@@ -1,13 +1,63 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 from .models import *
 
 
 @admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
+class ItemAdmin(ModelAdmin):
     list_display = ('id', 'name', 'price', 'description')
     list_display_links = ('id', 'name')
     search_fields = ('id', 'name', 'price')
     list_filter = ('name', 'price')
     fields = ('name', 'description', 'price')
     readonly_fields = ('id',)
+    ordering = ['name', 'price']
     save_on_top = True
+
+
+@admin.register(Order)
+class OrderAdmin(ModelAdmin):
+    list_display = ('number', 'items_list', 'discount_types', 'total_discount', 'total_tax', 'total_price')
+    list_display_links = ('number',)
+    search_fields = ('number',)
+    ordering = ['number']
+    save_on_top = True
+
+    def items_list(self, obj):
+        return ", ".join([f"id: {item.item.pk} (q: {item.quantity})" for item in obj.orderitem_set.all()])
+
+    def total_tax(self, obj):
+        return obj.total_tax()
+
+    def total_discount(self, obj):
+        return obj.total_discount()
+
+    def discount_types(self, obj):
+        return ", ".join([discount.name for discount in obj.discounts.all()])
+
+    items_list.short_description = 'Items'  # Название для колонки в списке
+    total_tax.short_description = 'Total Tax'
+    total_discount.short_description = 'Total Discount'
+    discount_types.short_description = 'Discount Types'
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(ModelAdmin):
+    list_display = ('order', 'item', 'quantity')
+    ordering = ['order', 'quantity', 'item']
+
+
+@admin.register(Discount)
+class DiscountAdmin(ModelAdmin):
+    list_display = ('name', 'percentage')
+    list_display_links = ('name', 'percentage')
+    fields = ('name', 'percentage')
+    ordering = ['percentage', 'name']
+
+
+@admin.register(Tax)
+class TaxAdmin(ModelAdmin):
+    list_display = ('name', 'percentage')
+    list_display_links = ('name', 'percentage')
+    fields = ('name', 'percentage')
+    ordering = ['percentage', 'name']
--- Shop_project/shop/models.py
+++ Shop_project/shop/models.py
@@ -1,14 +1,63 @@
-from django.db.models import Model, CharField, TextField, DecimalField
+from django.db.models import Model, CharField, TextField, DecimalField, ManyToManyField, \
+    PositiveIntegerField, PositiveSmallIntegerField, ForeignKey, PROTECT
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 # Create your models here.
 class Item(Model):
     name = CharField(max_length=70)
     description = TextField()
-    price = DecimalField(max_digits=10, decimal_places=2)
+    price = DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
 
     def __str__(self):
-        return f"[id: {self.id}] [item: {self.name}]"
+        return f"[id: {self.pk}] [item: {self.name}]"
 
     def get_absolute_url(self):
-        return f'/item/{self.id}/'
+        return f'/item/{self.pk}/'
+
+
+class Discount(Model):
+    name = CharField(max_length=25)
+    percentage = PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def __str__(self):
+        return f'{self.name} discount {self.percentage} %'
+
+
+class Tax(Model):
+    name = CharField(max_length=25)
+    percentage = PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def __str__(self):
+        return f'{self.name} tax {self.percentage} %'
+
+
+class Order(Model):
+    number = CharField(max_length=25, unique=True)
+    items = ManyToManyField(Item, through='OrderItem')
+    discounts = ManyToManyField(Discount, blank=True)
+    taxes = ManyToManyField(Tax, blank=True)
+
+    def __str__(self):
+        return f'Order № {self.number}'
+
+    def total_tax(self):
+        return sum(tax.percentage for tax in self.taxes.all())
+
+    def total_discount(self):
+        return sum(discount.percentage for discount in self.discounts.all())
+
+    def total_price(self):
+        return sum(order_item.quantity * order_item.item.price for order_item in self.orderitem_set.all())
+
+    def final_price(self):
+        total_items_price = self.total_price()
+        final_price = total_items_price - (total_items_price * (self.total_discount() / 100)) + (
+                    total_items_price * (self.total_tax() / 100))
+        return final_price
+
+
+class OrderItem(Model):
+    order = ForeignKey(Order, on_delete=PROTECT)
+    item = ForeignKey(Item, on_delete=PROTECT)
+    quantity = PositiveIntegerField()
